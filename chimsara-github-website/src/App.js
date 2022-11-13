3c79e6f5f63ef9fed9a0efe94162fb24fec34832
chimsara <ericanwoga@gmail.com>
2022-11-13 17:12:47 -0500

    New background image

--- chimsara-github-website/src/App.js
+++ chimsara-github-website/src/App.js
@@ -7,7 +7,7 @@ import Experience from './components/experience/Experience';
 import Work from './components/work/Work'
 import Contact from './components/contact/Contact';
 import Footer from './components/footer/Footer';
-import Header from './components/header/Header';
+import Title from './components/title/Title';
 
 
 
@@ -15,10 +15,8 @@ function App() {
   return (
     <div >
       <Router>
-        <header>
-          <Nav/>
-          <Header/>
-        </header>
+        <Nav/>
+        <Title/>
         <Home/>
         <About/>
         <Work />
---
+++ chimsara-github-website/src/components/title/Title.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './title.css'
+
+function Title() {
+  return (
+        <div className="title">
+            
+        </div>
+  )
+}
+
+export default Title
