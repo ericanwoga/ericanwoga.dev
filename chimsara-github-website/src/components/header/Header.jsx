38c60046496f17f2cfa28f7dfd66548a506cbdeb
chimsara <ericanwoga@gmail.com>
2022-11-13 16:54:33 -0500

    Deleted old website layout and creating a new layout

--- chimsara-github-website/src/components/header/Header.jsx
+++ chimsara-github-website/src/components/header/Header.jsx
@@ -3,22 +3,8 @@ import './header.css'
 
 function Header() {
   return (
-        <div className="container header__container">
-            <div class="home-title">
-                <h1 id="home-title__hi">Hi, my name is </h1>
-                <h1 id="home-title__erica">
-                    <span>E</span>
-                    <span>r</span>
-                    <span>i</span>
-                    <span>c</span>
-                    <span>a</span>
-                    </h1>
-                {//<h1 id="home-title__hi">Hi, my name is </h1><h1 id="home-title__erica">Erica Nwoga</h1>
-                }
-            </div>
-            <h2>I create lovely frontend products that are simple and 
-                UI friendly.
-            </h2>
+        <div className="header__container">
+            
         </div>
   )
 }
--- chimsara-github-website/src/components/nav/Nav.jsx
+++ chimsara-github-website/src/components/nav/Nav.jsx
@@ -4,12 +4,13 @@ import './nav.css'
 function Nav() {
   return (
     <nav className="nav">
+      {/*
         <ul>
             <li><a href="#">home</a></li>
             <li><a href="#about">about</a></li>
             <li><a href="#work">portfolio</a></li>
             <li><a href="#contact">contact</a></li>
-        </ul>
+  </ul> */}
     </nav>
   )
 }
