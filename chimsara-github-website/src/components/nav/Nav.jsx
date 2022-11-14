1c986e45b16fefa8ff0d8e1795adc1681997a84b
chimsara <ericanwoga@gmail.com>
2022-11-14 02:15:27 -0500

    Finished Nav Bar

--- chimsara-github-website/src/components/nav/Nav.jsx
+++ chimsara-github-website/src/components/nav/Nav.jsx
@@ -1,12 +1,19 @@
-import React , {useState } from 'react'
+import React , {useState, useRef } from 'react'
 import './nav.css'
 import {IoMenu, IoClose} from 'react-icons/io5'
 
 
 function Nav() {
   const [clickedNav, setClickedNav] = useState(false);
-  const handleClick = () => {
+
+  const switchIcon = () => {
     setClickedNav(!clickedNav);
+    toggleNav();
+  }
+
+  const toggleNav = () => {
+    const navbar = document.querySelector(".navbar-links");
+    navbar.classList.toggle("active");
   }
   
   return (
@@ -14,10 +21,6 @@ function Nav() {
     Navbar-open is for smaller devices*/
     <>
       <nav className="navbar">
-      {clickedNav ? 
-      <IoClose className='menu-icon-open' onClick={handleClick}/> :
-      <IoMenu className='menu-icon-closed' onClick={handleClick}/>}
-
       <div className='navbar-links'>
         <a href="#" className="navbar-item">home</a>
         <a href="#about" className="navbar-item">about</a>
@@ -25,11 +28,9 @@ function Nav() {
         <a href="#contact" className="navbar-item">contact</a>
       </div>
 
-        <div className="menu-dropdown">
-          <span class="dropdown-item"></span>
-          <span class="dropdown-item"></span>
-          <span class="dropdown-item"></span>
-        </div>  
+      {clickedNav ? 
+      <IoClose className='menu-icon-open' onClick={switchIcon}/> :
+      <IoMenu className='menu-icon-closed' onClick={switchIcon}/>}
     </nav>
     
     </>
