9737d97f1b495a193b142e10e71e7d9d5234bfab
chimsara <ericanwoga@gmail.com>
2022-11-13 22:07:03 -0500

    Fixed nav bar size

--- chimsara-github-website/src/components/nav/Nav.jsx
+++ chimsara-github-website/src/components/nav/Nav.jsx
@@ -1,14 +1,25 @@
-import React from 'react'
+import React , {useState } from 'react'
 import './nav.css'
+import {IoMenu, IoClose} from 'react-icons/io5'
 
 
 function Nav() {
+  const [clickedNav, setClickedNav] = useState(false);
+  const handleClick = () => {
+    setClickedNav(!clickedNav);
+  }
+  
   return (
-  <nav className="navbar">
+  <nav className={clickedNav ? "navbar-open" : "navbar" } >
+    {clickedNav ? 
+    <IoClose className='menu-icon-open' onClick={handleClick}/> :
+    <IoMenu className='menu-icon-closed' onClick={handleClick}/>}
+
     <a href="#">home</a>
     <a href="#about">about</a>
     <a href="#work">portfolio</a>
     <a href="#contact">contact</a>
+    
   </nav>
   )
 }
