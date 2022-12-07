328f6b53dfd3a5aec28283ebe4b02fbce92faf24
chimsara <ericanwoga@gmail.com>
2022-12-06 23:04:53 -0500

    Edited title

--- chimsara-github-website/src/components/nav/Nav.jsx
+++ chimsara-github-website/src/components/nav/Nav.jsx
@@ -22,9 +22,9 @@ function Nav() {
     <>
       <nav className="navbar">
         <ul className='navbar-links'>
-          <li><a href="#" className="navbar-item">home</a></li>
+          <li><a href="#home" className="navbar-item">home</a></li>
           <li><a href="#about" className="navbar-item">about</a></li>
-          <li><a href="#work" className="navbar-item">portfolio</a></li>
+          <li><a href="#portfolio" className="navbar-item">portfolio</a></li>
           <li><a href="#contact" className="navbar-item">contact</a></li>
         </ul>
 
