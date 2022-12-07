fa62865c6985b094350e57ad28c08629e81f5890
chimsara <ericanwoga@gmail.com>
2022-12-06 22:38:19 -0500

    Edited title

--- chimsara-github-website/src/components/home/Home.jsx
+++ chimsara-github-website/src/components/home/Home.jsx
@@ -6,15 +6,15 @@ import Title from '../title/Title'
 function Home() {
   return (
     <section id="home">
-      
+    <div id="entire-title">
       <h1>Hi! I'm Erica</h1>
       <div id="subtitle-text">
-        <h2>An aspiring front-end
-          <span className='deco-subtitle-text'>   developer </span></h2> 
-        <h2>with appreciation for user-friendly
-          <span className='deco-subtitle-text'>   design</span></h2>
-      </div>
-      <img src={bunny} id="bunny"/>
+        <h2>An aspiring front-end 
+          <span className='deco-subtitle-text'> developer </span></h2> 
+        <h2>with appreciation for user-friendly 
+          <span className='deco-subtitle-text'> design</span></h2>
+    </div>
+    </div>   
     </section>
   )
 }
