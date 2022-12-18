9ecb945882c1f4de9c6054875eccf72f094b01f4
chimsara <ericanwoga@gmail.com>
2022-12-17 21:56:15 -0500

    Added Contact section

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
 import './contact.css'
+import {IoLogoLinkedin, IoMail} from 'react-icons/io5'
+import {GoMarkGithub} from 'react-icons/go'
 
 function Contact() {
   return (
     <section id="contact">
-      
+      <h1 className='section-header'>Let's Connect</h1>
+      <div className='contact-content'>
+        <div className='contact-list'>
+          <span><IoLogoLinkedin className='linkedin-icon' onClick=""/> </span>
+          <span><GoMarkGithub className='github-icon' onClick=""/> </span>
+          <span><IoMail className='email-icon' onClick=""/> </span>
+        </div>
+
+      </div> 
     </section>
   )
 }
