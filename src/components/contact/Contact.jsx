f1b40a8061ce83a952ba0ccd4489e86e5fc4c180
Erica Nwoga <66533300+ericanwoga@users.noreply.github.com>
2024-06-03 18:44:21 -0400

    Update Contact.jsx

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+iimport React from 'react'
 import './contact.css'
 import {IoLogoLinkedin, IoMail} from 'react-icons/io5'
 import {GoMarkGithub} from 'react-icons/go'
@@ -9,15 +9,15 @@ function Contact() {
       <h1 className='section-header'>Let's Connect</h1>
       <div className='contact-content'>
         <div className='contact-list'>
-          {//<a href='https://www.linkedin.com/in/chimsara/' target="_blank" rel="noopener noreferrer"><IoLogoLinkedin className='linkedin-icon'/> </a>
-          //<a href="https://github.com/chimsara" target="_blank" rel="noopener noreferrer"><GoMarkGithub className='github-icon'/> </a>
-          //<a href="mailto: chimsarae@gmail.com" target="_blank" rel="noopener noreferrer"><IoMail className='email-icon'/> </a>
+          {//<a href='https://www.linkedin.com/in/ericanwoga/' target="_blank" rel="noopener noreferrer"><IoLogoLinkedin className='linkedin-icon'/> </a>
+          //<a href="https://github.com/ericanwoga" target="_blank" rel="noopener noreferrer"><GoMarkGithub className='github-icon'/> </a>
+          //<a href="mailto: nwoga.erica@gmail.com" target="_blank" rel="noopener noreferrer"><IoMail className='email-icon'/> </a>
         }
-          <a href='https://www.linkedin.com/in/chimsara/' target="_blank" rel="noopener noreferrer" className='linkedin-icon'>
+          <a href='https://www.linkedin.com/in/ericanwoga/' target="_blank" rel="noopener noreferrer" className='linkedin-icon'>
             <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/linkedin/linkedin-original.svg" alt="linkedin" title="LinkedIn"/>
           </a>
-          <a href="mailto: chimsarae@gmail.com" target="_blank" rel="noopener noreferrer"><IoMail className='email-icon'/></a>
-          <a href="https://github.com/chimsara" target="_blank" rel="noopener noreferrer"><GoMarkGithub className='github-icon'/> </a>
+          <a href="mailto: nwoga.erica@gmail.com" target="_blank" rel="noopener noreferrer"><IoMail className='email-icon'/></a>
+          <a href="https://github.com/ericanwoga" target="_blank" rel="noopener noreferrer"><GoMarkGithub className='github-icon'/> </a>
 
         </div>
 
